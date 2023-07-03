85b47e0886a4bc745f0ee78b65649b86df5a4ded
KeitaTsukuba <keitainterncoins@gmail.com>
2023-07-04 03:59:41 +0900

    first Commit.

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),  //小さすぎ！
+        Ordering::Greater => println!("Too big!"), //大きすぎ！
+        Ordering::Equal => println!("You win!"),   //やったね！
+    }
+}
